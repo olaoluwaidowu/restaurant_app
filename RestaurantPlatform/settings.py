c5ad55fa6ee52cbc04b1a5377ea1d33429929032
Olaoluwa <olaoluwaidowu22@gmail.com>
2023-11-06 05:06:40 +0100

    fix: password revoked

--- RestaurantPlatform/settings.py
+++ RestaurantPlatform/settings.py
@@ -139,7 +139,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'hawleyclef@gmail.com'
-EMAIL_HOST_PASSWORD = 'qatjihixjcmyowjd'  
+EMAIL_HOST_PASSWORD = 'qatjihixjcmyowjd'  # Revoked
 
 #REQUESTS_CA_BUNDLE = certifi.where()
 
